255647489dc89fdf940c00ce3355ea730ed025ca
NotPedrovisk <not.pedrovisk69@gmail.com>
2025-10-08 09:48:23 -0400

    added script to check if passwords are the same, no patterns needed though

--- scripts.js
+++ scripts.js
@@ -1,1 +1,27 @@
 console.log("sus")
+
+pass = document.getElementById("user_pass");
+confPass = document.getElementById("user_passConf")
+
+form = document.getElementById("formZone")
+
+console.log(form)
+
+function checkPass(){
+    if(pass.value.trim() == confPass.value.trim()){
+        confPass.setCustomValidity("")
+        return true
+    }
+    else{
+        console.log("WRONG")
+        confPass.setCustomValidity("Passwords don't match")
+        return false
+    }
+}
+
+confPass.addEventListener("input",(event)=>{
+
+    const isSame = checkPass();
+
+}
+)
